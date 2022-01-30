ffc7f1d4387239f5904e9f28ce8d61b9ab35bf0b
Todd Liebenschutz-Jones <dev@toddljones.me>
2022-01-30 12:58:57 +0000

    Add more test cases

--- game/GuessTest.py
+++ game/GuessTest.py
@@ -37,6 +37,61 @@ class GuessTest(unittest.TestCase):
 
         self.assertEqual(Guess.evaluate_guess(guess, target).matches, matches)
 
+    def testEvaluatesLettersInWrongPlaceWithMultipleRepeats(self):
+        guess = "hello"
+        target = "world"
+        matches = [Guess.Match.NONE,
+                   Guess.Match.NONE,
+                   Guess.Match.LETTER,
+                   Guess.Match.NONE,
+                   Guess.Match.LETTER]
+
+        self.assertEqual(Guess.evaluate_guess(guess, target).matches, matches)
+
+    def testEvaluatesLettersInWrongPlace(self):
+        guess = "world"
+        target = "hello"
+        matches = [Guess.Match.NONE,
+                   Guess.Match.LETTER,
+                   Guess.Match.NONE,
+                   Guess.Match.LETTER,
+                   Guess.Match.NONE]
+
+        self.assertEqual(Guess.evaluate_guess(guess, target).matches, matches)
+
+    def testEvaluatesLettersInWrongPlaceWithSomeFullMatches(self):
+        guess = "bowls"
+        target = "world"
+        matches = [Guess.Match.NONE,
+                   Guess.Match.LETTER_PLACE,
+                   Guess.Match.LETTER,
+                   Guess.Match.LETTER_PLACE,
+                   Guess.Match.NONE]
+
+        self.assertEqual(Guess.evaluate_guess(guess, target).matches, matches)
+
+    def testEvaluatesTwoLettersInWrongPlace(self):
+        guess = "boots"
+        target = "taboo"
+        matches = [Guess.Match.LETTER,
+                   Guess.Match.LETTER,
+                   Guess.Match.LETTER,
+                   Guess.Match.LETTER,
+                   Guess.Match.NONE]
+
+        self.assertEqual(Guess.evaluate_guess(guess, target).matches, matches)
+
+    def testEvaluatesLettersInWrongPlaceAndFullMatchForSameLetter(self):
+        guess = "maims"
+        target = "gamma"
+        matches = [Guess.Match.LETTER,
+                   Guess.Match.LETTER_PLACE,
+                   Guess.Match.NONE,
+                   Guess.Match.LETTER_PLACE,
+                   Guess.Match.NONE]
+
+        self.assertEqual(Guess.evaluate_guess(guess, target).matches, matches)
+
 
 if __name__ == '__main__':
     unittest.main()

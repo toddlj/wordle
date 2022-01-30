5f3d5b52fcdc4368efcfec19db82bb257c090f80
Todd Liebenschutz-Jones <dev@toddljones.me>
2022-01-30 15:13:25 +0000

    Rename variable

--- player/util/PossibleTargetFilter.py
+++ player/util/PossibleTargetFilter.py
@@ -4,10 +4,10 @@ from lib.words import targetlist
 
 class PossibleTargetFilter:
     def apply(self, history: [Guess]) -> {""}:
-        possible_words = targetlist
+        possible_targets = targetlist
         for guess in history:
-            possible_words = filter(
+            possible_targets = filter(
                 lambda word: Guess.evaluate_guess(guess.word, word).matches == guess.matches,
-                possible_words
+                possible_targets
             )
-        return possible_words
+        return possible_targets

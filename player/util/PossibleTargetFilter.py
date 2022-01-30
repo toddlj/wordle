cca0d6b1f7df0d9f4f4b709b6982126aab6d053d
Todd Liebenschutz-Jones <dev@toddljones.me>
2022-01-30 16:43:41 +0000

    Only evaluate new words

--- player/util/PossibleTargetFilter.py
+++ player/util/PossibleTargetFilter.py
@@ -7,29 +7,28 @@ class PossibleTargetFilter:
         self.possible_targets: {""} = targetlist
 
     def apply(self, history: [Guess]) -> None:
-        new_words = [history[-1]] if len(history) > 0 else []
+        new_word = history[-1] if len(history) > 0 else None
 
-        self.filter_known_letters_without_counts(new_words)
-        self.filter_known_letter_places(new_words)
-        self.filter_guess_outcomes(new_words)
+        if new_word:
+            self.filter_known_letters_without_counts(new_word)
+            self.filter_known_letter_places(new_word)
+            self.filter_guess_outcomes(new_word)
 
-    def filter_known_letters_without_counts(self, history: [Guess]):
+    def filter_known_letters_without_counts(self, guess: Guess):
         known_letters = set()
-        for guess in history:
-            for i in range(5):
-                if guess.matches[i] in {Guess.Match.LETTER_PLACE, Guess.Match.LETTER}:
-                    known_letters.add(guess.word[i])
+        for i in range(5):
+            if guess.matches[i] in {Guess.Match.LETTER_PLACE, Guess.Match.LETTER}:
+                known_letters.add(guess.word[i])
         self.possible_targets = set(filter(
             lambda word: set(word).issuperset(known_letters),
             self.possible_targets
         ))
 
-    def filter_known_letter_places(self, history: [Guess]):
+    def filter_known_letter_places(self, guess: Guess):
         known_letter_places = {}
-        for guess in history:
-            for i in range(5):
-                if guess.matches[i] == Guess.Match.LETTER_PLACE:
-                    known_letter_places[i] = guess.word[i]
+        for i in range(5):
+            if guess.matches[i] == Guess.Match.LETTER_PLACE:
+                known_letter_places[i] = guess.word[i]
 
         for position in known_letter_places.keys():
             self.possible_targets = set(filter(
@@ -37,9 +36,8 @@ class PossibleTargetFilter:
                 self.possible_targets
             ))
 
-    def filter_guess_outcomes(self, history):
-        for guess in history:
-            self.possible_targets = set(filter(
-                lambda word: Guess.evaluate_guess(guess.word, word).matches == guess.matches,
-                self.possible_targets
-            ))
+    def filter_guess_outcomes(self, guess: Guess):
+        self.possible_targets = set(filter(
+            lambda word: Guess.evaluate_guess(guess.word, word).matches == guess.matches,
+            self.possible_targets
+        ))
